1b03c89dc7bd595dba3543af5b042c219d6f9fae
rangapv <rangapv@yahoo.com>
2023-03-29 06:56:45 +0000

    Script to Install Ray Operator & Cluster on to a Kubernetes Setup

---
+++ rayinstall.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#Author: rangapv@yahoo.com 29-03-23
+
+ray=1
+raya=1
+
+inst() {
+
+kcl=`kubectl get nodes`
+kcls="$?"
+
+if [[ (( $kcls -ne 0 )) ]]
+then
+	echo "k8s Cluster Not Ready"
+	exit
+fi
+
+
+ray11=`kubectl -n ray-system get pod --selector=app.kubernetes.io/component=kuberay-operator`
+ray11s="$?"
+
+if [[ (( $ray11s -eq 0 )) ]]
+then
+  status $ray11s ${ray11}
+else
+  ray1=`kubectl create -k "github.com/ray-project/kuberay/ray-operator/config/default?ref=v0.4.0&timeout=90s"`
+  ray1s="$?"
+  ray=0
+  status $ray1s $ray1
+fi
+
+ray10=`kubectl get raycluster`
+ray10s="$?"
+
+if [[ (( $ray10s -eq 0 )) ]]
+then
+ray=0
+raya=0
+status $ray10s $ray10
+else 
+ray2=`kubectl apply -f https://raw.githubusercontent.com/ray-project/kuberay/master/ray-operator/config/samples/ray-cluster.autoscaler.yaml`
+ray2s="$?"
+raya=0
+status $ray2s $ray2
+fi
+
+
+if [[ (( $ray -eq 0 )) && (( $raya -eq 0 )) ]]
+then
+ray3=`kubectl get pods --selector=ray.io/cluster=raycluster-autoscaler`
+echo "ray3 is $ray3"
+ray3s="$?"
+fi
+
+}
+
+
+status() {
+
+st=$1
+cmd=$2
+
+if [[ (( $st -eq 0 )) ]]
+then
+	echo "the command $cmd PASSED"
+else
+	echo "the command $cmd FAILED"
+fi
+
+}
+
+
+inst
