b5acfbb9c2fc32e98b8234d36cec5a4cc3257fc1
rangapv <rangapv@yahoo.com>
2023-08-28 11:05:30 +0000

    Fixed the ray-auto-scaler YAML and some minor code changes

--- rayinstall.sh
+++ rayinstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #Author: rangapv@yahoo.com 29-03-23
 
 ray=1
@@ -18,14 +18,14 @@ fi
 
 ray11=`kubectl -n ray-system get pod --selector=app.kubernetes.io/component=kuberay-operator | wc -l`
 ray11s="$?"
-if [[ (( $ray11 -gt 0 )) ]]
+if [[ ( "$ray11" -gt 0 ) ]]
 then
-  status $ray11s $ray11
+  status "$ray11s" "$ray11"
 else
   ray1=`kubectl create -k "github.com/ray-project/kuberay/ray-operator/config/default?ref=v0.4.0&timeout=90s"`
   ray1s="$?"
   ray=0
-  status $ray1s $ray1
+  status "$ray1s" "$ray1"
 fi
 
 ray10=`kubectl get raycluster | wc -l`
@@ -37,7 +37,8 @@ ray=0
 raya=0
 #status $ray10s $ray10
 else 
-ray2=`kubectl apply -f https://raw.githubusercontent.com/ray-project/kuberay/master/ray-operator/config/samples/ray-cluster.autoscaler.yaml`
+ray2=`kubectl apply -f https://raw.githubusercontent.com/rangapv/Rayapp/rayauto.yaml`
+#ray2=`kubectl apply -f https://raw.githubusercontent.com/ray-project/kuberay/master/ray-operator/config/samples/ray-cluster.autoscaler.yaml`
 ray2s="$?"
 raya=0
 status $ray2s $ray2
@@ -46,10 +47,19 @@ fi
 
 if [[ (( $ray -eq 0 )) && (( $raya -eq 0 )) ]]
 then
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo "Printing Ray-Cluster Status"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ray32=`kubectl get raycluster`
 echo "$ray32"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo "Printing Ray-Pod Status"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ray31=`kubectl -n ray-system get pod --selector=app.kubernetes.io/component=kuberay-operator`	
 echo "$ray31"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo "Printing Ray-AutoScaler Status"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ray3=`kubectl get pods --selector=ray.io/cluster=raycluster-autoscaler`
 echo "$ray3"
 ray3s="$?"
@@ -62,7 +72,6 @@ status() {
 
 st=$1
 cmd=$2
-
 if [[ (( $st -eq 0 )) ]]
 then
 	echo "the command $cmd PASSED"
